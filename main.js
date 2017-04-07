77b789941aa54277b0369d9cd573243a526d2adb
afrila <afrilabimaris@gmail.com>
2017-04-07 18:21:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+$(function() {
+	var navigasi = $('nav').offset().top;
+	var sticky = function(){
+		var scrolltop = $(window).scrollTop();
+
+		if (scrolltop > navigasi) 
+		{
+			$('nav').addClass('fixed');
+		}else
+		{
+			$('nav').removeClass('fixed');
+		}
+
+	};
+	sticky();
+	$(window).scroll(function(){
+		sticky();
+	})
+})
